8dd2e8111301321d6d79d4e6c23bbbdb8d33fb10
akbarrmn <akbar.rahmana@gmail.com>
2022-08-29 12:15:19 +0700

    fixing container

--- src/Components/Containers/Container.jsx
+++ src/Components/Containers/Container.jsx
@@ -1,10 +1,12 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Container = (props) => {
+  const location = useLocation().pathname
   return (
     <>
-        <Box sx={{ width: '428px',height:'100%', minHeight: '100vh', mr:'auto', ml: 'auto', backgroundColor:'white', position:'relative' }}>
+        <Box sx={{  width: location === '/'?'unset': '428px',maxWidth:'428px', height:'100%', minHeight: '100vh', mr:'auto', ml: 'auto', backgroundColor:'white', position:'relative' }}>
             {props.children}
         </Box>
     </>
