4c637bdea435e8fc1a9771df33fef045bdf0e81f
akbarrmn <akbar.rahmana@gmail.com>
2022-11-23 23:16:36 +0700

    Fixing components

--- src/Pages/Transaction/Transaction.jsx
+++ src/Pages/Transaction/Transaction.jsx
@@ -1,4 +1,4 @@
-import { Box, Fab, InputAdornment, OutlinedInput, Typography } from '@mui/material'
+import { Box, Fab, InputAdornment, OutlinedInput, Tab, Typography } from '@mui/material'
 import React from 'react'
 import BottomBar from '../../Components/BottomBar/BottomBar'
 import Container from '../../Components/Containers/Container'
@@ -8,28 +8,31 @@ import foto from "../../Assets/image.jpg"
 import AddIcon from '@mui/icons-material/Add';
 import { useNavigate } from 'react-router-dom';
 import Balance from '../../Components/Balance/Balance';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 
 const Transaction = () => {
-  const [clicked, setClicked] = React.useState(localStorage.getItem("transaction"))
+  const [clicked, setClicked] = React.useState("1")
   const navigate = useNavigate()
   const add = () => {
-    clicked === "Pengeluaran" ? navigate('/tambah-pengeluaran') : navigate('/tambah-pemasukan')
-  }
+    if (clicked === '1') {
+      window.localStorage.setItem("clicked", '1')
+      navigate('/tambah-pengeluaran')
+    }else{
+      window.localStorage.setItem("clicked", '2')
+      navigate('/tambah-pemasukan')
+    }
+  } 
 
-  React.useEffect(() => {
-    return () => {
-      const getItem = localStorage.getItem("transaction")
-      if (getItem === null) {
-        setClicked("Pengeluaran")
-      } else {
-        if (clicked !== "Pemasukan") {
-          localStorage.setItem("transaction", "Pengeluaran")
-        } else {
-          localStorage.setItem("transaction", "Pemasukan")
-        }
-      }
-    };
-  }, [clicked]);
+  React.useEffect(()=>{
+    const clickedStorage = window.localStorage.getItem("clicked")
+    if (clickedStorage) {
+      setClicked(clickedStorage)
+    }
+  },[])
+
+  const handleChange = (event, newValue) => {
+    setClicked(newValue);
+  };
 
   return (
     <>
@@ -37,137 +40,108 @@ const Transaction = () => {
         <Box pb={'180px'}>
           <Balance />
         </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', gap: '24px', height: '24px', py: 3, width: '100%' }}>
-          <Box onClick={() => setClicked('Pengeluaran')} sx={{ display: 'flex', cursor: 'pointer', flexDirection: 'column', alignItems: 'center', gap: '8px', position: 'relative', width: '165px', height: '40px' }}>
-            <Typography sx={{ color: clicked === 'Pengeluaran' ? 'black' : '#B2B3B3' }}>Pengeluaran</Typography>
-            <Box sx={{
-              display: clicked === 'Pengeluaran' ? 'block' : 'none',
-              background: '#191C1B',
-              width: '100%',
-              height: '4px',
-              borderRadius: '8px',
-              bottom: 0,
-              position: 'absolute'
-            }} />
-          </Box>
-          <Box onClick={() => setClicked('Pemasukan')}
-            sx={{
+        <TabContext value={clicked}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', gap: '24px', height: '24px', py: 3, width: '100%' }}>
+            <Tab label="Pengeluaran" value="1" sx={{ color: clicked === '1' ? 'black' : '#B2B3B3', cursor: 'pointer',  alignItems: 'center', gap: '8px', position: 'relative', width: '50%', height: '40px' }} />
+            <Tab label="Pemasukan" value="2" sx={{ color: clicked === '2' ? 'black' : '#B2B3B3', display: 'flex', cursor: 'pointer', flexDirection: 'column', alignItems: 'center', gap: '8px', position: 'relative', width: '50%', height: '40px' }} />
+          </TabList>
+          <Box sx={{ display: 'flex', gap: '8px', mx: 2 }}>
+            <OutlinedInput
+              startAdornment={<InputAdornment position="start"><SearchIcon /></InputAdornment>}
+              id="outlined-basic"
+              variant="outlined"
+              placeholder='Cari transaksi'
+              sx={{ height: '50px' }}
+              fullWidth />
+            <Box tabIndex={1} sx={{
+              border: '1px solid #B2B3B3',
+              justifyContent: 'space-around',
+              p: '0px 8px',
+              width: '81px',
+              height: '48px',
               display: 'flex',
-              cursor: 'pointer',
-              flexDirection: 'column',
               alignItems: 'center',
-              gap: '8px',
-              position: 'relative',
-              width: '165px',
-              height: '40px'
-            }}>
-            <Typography sx={{ color: clicked === 'Pemasukan' ? 'black' : '#B2B3B3' }}>Pemasukan</Typography>
-            <Box sx={{
-              display: clicked === 'Pemasukan' ? 'block' : 'none',
-              background: '#191C1B',
-              width: '100%',
-              height: '4px',
               borderRadius: '8px',
-              bottom: 0,
-              position: 'absolute'
-            }} />
-          </Box>
-        </Box>
-        <Box sx={{ display: 'flex', gap: '8px', mx: 2 }}>
-          <OutlinedInput
-            startAdornment={<InputAdornment position="start"><SearchIcon /></InputAdornment>}
-            id="outlined-basic"
-            variant="outlined"
-            placeholder='Cari transaksi'
-            sx={{ height: '50px' }}
-            fullWidth />
-          <Box tabIndex={1} sx={{
-            border: '1px solid #B2B3B3',
-            justifyContent: 'space-around',
-            p: '0px 8px',
-            width: '81px',
-            height: '48px',
-            display: 'flex',
-            alignItems: 'center',
-            borderRadius: '8px',
-            cursor: 'pointer',
-            ':focus': {
-              border: '1px solid black'
-            }
-          }}>
-            <FilterAltIcon />
-            <Typography sx={{ fontSize: '14px', fontWeight: 400 }}>
-              Filter
-            </Typography>
-          </Box>
-        </Box>
-        {clicked === 'Pengeluaran' ?
-          <Box className='transaction-content' sx={{ mx: 2, my: 2 }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
-                <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
-                </Box>
-              </Box>
-              <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#FF0000' }}>-Rp. 50.000</Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
-                <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
-                </Box>
-              </Box>
-              <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#FF0000' }}>-Rp. 50.000</Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
-                <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
-                </Box>
-              </Box>
-              <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#FF0000' }}>-Rp. 50.000</Typography>
+              cursor: 'pointer',
+              ':focus': {
+                border: '1px solid black'
+              }
+            }}>
+              <FilterAltIcon />
+              <Typography sx={{ fontSize: '14px', fontWeight: 400 }}>
+                Filter
+              </Typography>
             </Box>
           </Box>
-          :
-          <Box className='transaction-content' sx={{ mx: 2, my: 2 }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
-                <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+          <TabPanel value="1">
+            <Box className='transaction-content' sx={{ mx: 2, my: 2 }}>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
+                  <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+                  </Box>
                 </Box>
+                <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#FF0000' }}>-Rp. 50.000</Typography>
               </Box>
-              <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#2FAA23' }}>+Rp. 50.000</Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
-                <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
+                  <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+                  </Box>
                 </Box>
+                <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#FF0000' }}>-Rp. 50.000</Typography>
               </Box>
-              <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#2FAA23' }}>+Rp. 50.000</Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
-                <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
-                  <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
+                  <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+                  </Box>
                 </Box>
+                <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#FF0000' }}>-Rp. 50.000</Typography>
               </Box>
-              <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#2FAA23' }}>+Rp. 50.000</Typography>
             </Box>
-          </Box>
-        }
+          </TabPanel>
+          <TabPanel value="2">
+            <Box className='transaction-content' sx={{ mx: 2, my: 2 }}>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
+                  <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+                  </Box>
+                </Box>
+                <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#2FAA23' }}>+Rp. 50.000</Typography>
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
+                  <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+                  </Box>
+                </Box>
+                <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#2FAA23' }}>+Rp. 50.000</Typography>
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '12px' }}>
+                  <Box component={'img'} src={foto} sx={{ width: '56px', height: '56px', borderRadius: '50%' }} />
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500 }}>Uang Saku</Typography>
+                    <Typography sx={{ fontSize: '14px', fontWeight: 500, color: '#8C8D8D' }}>29 Agustus 2022</Typography>
+                  </Box>
+                </Box>
+                <Typography sx={{ fontSize: '16px', fontWeight: 500, color: '#2FAA23' }}>+Rp. 50.000</Typography>
+              </Box>
+            </Box>
+          </TabPanel>
+        </TabContext>
         <Fab color="primary" aria-label="add" onClick={add} sx={{ bottom: '130px', right: '20px', position: 'absolute' }}>
           <AddIcon />
         </Fab>
